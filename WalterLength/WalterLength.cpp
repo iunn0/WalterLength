f64f3534eedcf27be63d6f6c953793d7589906fe
iunn0 <bvip287@gmail.com>
2023-08-17 22:21:25 +0300

    “Update

--- WalterLength/WalterLength.cpp
+++ WalterLength/WalterLength.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int m_strlen(const char* str) {
     int length = 0;
-    for (int i = 0; i < strlen(str); i++) {    // спочатку зробив i < 20, потім знайшов що можна викликати strlen(const char* str);
-       if(str[i] != '\0') {
-            length++;
-        }
+    while (str[length] != '\0') {
+        length++;
     }
     return length;
 }
